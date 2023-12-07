67227df3e2f3c1c11190edb8d8923fef5126b33f
comsompom <gelfrad@yandex.com>
2023-12-07 16:06:48 +0200

    day 3 part 1 update

--- Advent_of_Code/2023/day_3/day_3_part_1.py
+++ Advent_of_Code/2023/day_3/day_3_part_1.py
@@ -1,8 +1,19 @@
+'''
+TBD Not Solved yet
+'''
 elf_file = 'elf_txt_3_1.txt'
-matrix = []
+matrix_symbols = []
+matrix_raw = []
 
 with open(elf_file) as elf_txt:
     for line in elf_txt.readlines():
-        matrix.append(line.replace('\n', '').split())
+        matrix_symbols.append(line.replace('\n', '').replace('.', ' ').split())
+        matrix_raw.append(line.replace('\n', '').replace('.', '_').split())
+    for idx, element in enumerate(matrix_raw):
+        for x in matrix_symbols[idx]:
+            check = element[0].find(x)
+            print(f'el_beg: {check}  el_end: {check + len(x) - 1}  curr_idx: {idx}')
 
-print(matrix)
+
+print(matrix_symbols)
+print(matrix_raw)
