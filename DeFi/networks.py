ce247208a506a234f5814c416d6e7b5d0e7e9b88
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-07 16:14:36 +0200

    add networks dataclass for switch between crypto networks

---
+++ DeFi/networks.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CryptoNetwork:
+    BNB: str = 'https://bsc-mainnet.infura.io/v3/'
+    ARBITRUM: str = 'https://arbitrum-mainnet.infura.io/v3/'
+    BASE: str = 'https://base-sepolia.infura.io/v3/'
+    BLAST: str = 'https://blast-mainnet.infura.io/v3/'
+    CELO: str = 'https://celo-mainnet.infura.io/v3/'
+    ETHERIUM: str = 'https://mainnet.infura.io/v3/'
+    LINEA: str = 'https://linea-mainnet.infura.io/v3/'
+    MANTLE: str = 'https://mantle-mainnet.infura.io/v3/'
+    opBNB: str = 'https://opbnb-mainnet.infura.io/v3/'
+    POLYGONPOS: str = 'https://polygon-mainnet.infura.io/v3/'
