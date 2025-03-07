f4c8bbfe1c83f2ee1eeaa5552241771c8a290ff4
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-07 13:54:41 +0200

    update example metamask operations with the wallet

--- DeFi/metamask_con.py
+++ DeFi/metamask_con.py
@@ -1,8 +1,19 @@
 from operations import MetamaskOperation
-from methods import EtheriumMethods
 
 
 address = "0x107119102c2EC84099cDce3D5eFDE2dcbf4DEB2a"
-operation = EtheriumMethods().get_balance
-mm_operation = MetamaskOperation(address, operation)
-print(mm_operation.get_balance())
+mm_wallet = MetamaskOperation(address)
+# return wallet balance
+print(f"Wallet: {address}, balance is: {mm_wallet.get_balance()}")
+
+# return current gas for transaction
+print(f"Current gas fee for transaction: {mm_wallet.get_gas_price()}")
+
+# return wallet transaction count
+print(f"Wallet: {address}, number transactions: {mm_wallet.get_transaction_count()}")
+
+# return chain id
+print(f"Chain ID: {mm_wallet.chain_id()}")
+
+# Returns an estimate of how much priority fee, in wei, you need to be included in a block.
+print(f"estimate of how much priority fee: {mm_wallet.max_priority_fee_per_gas()}")
