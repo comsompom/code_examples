af86d7588089866ff8a2c3cada9098533fbc804e
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-07 13:56:04 +0200

    refactor main operations class with extesion new methods

--- DeFi/operations.py
+++ DeFi/operations.py
@@ -2,31 +2,54 @@ import requests
 import json
 from constants import WEI, BASE_URL
 from utils import CryptoUtils, get_api_key
+from methods import EtheriumMethods
 
 
 class MetamaskOperation:
-    def __init__(self, wallet, operation):
+    def __init__(self, wallet):
         self.wallet = wallet
         self.api_key = get_api_key()
-        self.operation = operation
         self.headers = {"content-type": "application/json"}
         # possible blocks vals: latest, earliest, pending, safe, or finalized
         self.block = "latest"
+        self.url = f"{BASE_URL}{self.api_key}"
 
-    def requested_payload(self):
+    def requested_payload(self, operation, *param_args):
+        params = [param for param in param_args]
         return {
             "jsonrpc": "2.0",
-            "method": self.operation,
-            "params": [self.wallet, self.block],
+            "method": operation,
+            "params": params,
             "id": 1
         }
 
     def etherium_price(self):
         return CryptoUtils("ETH").get_price()
 
+    def response_from_request(self, payload):
+        return requests.post(self.url, data=json.dumps(payload), headers=self.headers).json()
+
     def get_balance(self):
-        url = f"{BASE_URL}{self.api_key}"
-        payload = self.requested_payload()
-        response = requests.post(url, data=json.dumps(payload), headers=self.headers).json()
-        balance = int(response['result'], 16) * WEI * float(self.etherium_price())
-        return balance
+        payload = self.requested_payload(EtheriumMethods().get_balance, self.wallet, self.block)
+        response = self.response_from_request(payload)
+        return int(response.get('result', 0.0), 16) * WEI * float(self.etherium_price())
+
+    def get_gas_price(self):
+        payload = self.requested_payload(EtheriumMethods().gas_price)
+        response = self.response_from_request(payload)
+        return int(response.get('result', 0.0), 16) * WEI * float(self.etherium_price())
+
+    def get_transaction_count(self):
+        payload = self.requested_payload(EtheriumMethods().get_transaction_count, self.wallet, self.block)
+        response = self.response_from_request(payload)
+        return int(response.get('result', 0.0), 16)
+
+    def chain_id(self):
+        payload = self.requested_payload(EtheriumMethods().chan_id)
+        response = self.response_from_request(payload)
+        return int(response.get('result', 0.0), 16)
+
+    def max_priority_fee_per_gas(self):
+        payload = self.requested_payload(EtheriumMethods().max_priority_fee_per_gas)
+        response = self.response_from_request(payload)
+        return int(response.get('result', 0.0), 16)
