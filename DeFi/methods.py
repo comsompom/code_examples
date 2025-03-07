21132748cd66cef43995921a6ff29bc22480068a
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-07 16:55:08 +0200

    change the web three method name class

--- DeFi/methods.py
+++ DeFi/methods.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class EtheriumMethods:
+class WebThreeMethods:
     get_balance: str = 'eth_getBalance'
     gas_price: str = 'eth_gasPrice'
     send_transaction: str = 'eth_sendTransaction'
