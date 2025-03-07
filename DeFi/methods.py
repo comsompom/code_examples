ffba64594da7b5a82544c18265a53a60fe271d4a
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-07 13:55:12 +0200

    extend web3 methods

--- DeFi/methods.py
+++ DeFi/methods.py
@@ -7,3 +7,6 @@ class EtheriumMethods:
     gas_price: str = 'eth_gasPrice'
     send_transaction: str = 'eth_sendTransaction'
     esimate_gas: str = 'eth_estimateGas'
+    get_transaction_count: str = 'eth_getTransactionCount'
+    chan_id: str = 'eth_chainId'
+    max_priority_fee_per_gas: str = 'eth_maxPriorityFeePerGas'
