2af56c4e20a5c600569a3810014154e6bb672704
comsompom <gelfrad@yandex.com>
2025-03-08 13:15:52 +0200

    refactor operation to privet methods for using outside just needed

--- DeFi/operations.py
+++ DeFi/operations.py
@@ -15,7 +15,7 @@ class MetamaskOperation:
         bnb_url = CryptoNetwork().BNB
         self.url = f"{bnb_url}{self.api_key}"
 
-    def requested_payload(self, operation, *param_args):
+    def _requested_payload(self, operation, *param_args):
         params = [param for param in param_args]
         return {
             "jsonrpc": "2.0",
@@ -24,34 +24,34 @@ class MetamaskOperation:
             "id": 1
         }
 
-    def etherium_price(self):
+    def _etherium_price(self):
         return CryptoUtils("ETH").get_price()
 
-    def response_from_request(self, payload):
+    def _response_from_request(self, payload):
         return requests.post(self.url, data=json.dumps(payload), headers=self.headers).json()
 
-    def response_to_usd(self, response):
-        return int(response.get('result', 0.0), 16) * WEI * float(self.etherium_price())
+    def _response_to_usd(self, response):
+        return int(response.get('result', 0.0), 16) * WEI * float(self._etherium_price())
 
-    def response_to_num(self, response):
+    def _response_to_num(self, response):
         return int(response.get('result', 0.0), 16)
 
     def get_balance(self):
-        payload = self.requested_payload(WebThreeMethods().get_balance, self.wallet, self.block)
-        return self.response_to_usd(self.response_from_request(payload))
+        payload = self._requested_payload(WebThreeMethods().get_balance, self.wallet, self.block)
+        return self._response_to_usd(self._response_from_request(payload))
 
     def get_gas_price(self):
-        payload = self.requested_payload(WebThreeMethods().gas_price)
-        return self.response_to_usd(self.response_from_request(payload))
+        payload = self._requested_payload(WebThreeMethods().gas_price)
+        return self._response_to_usd(self._response_from_request(payload))
 
     def get_transaction_count(self):
-        payload = self.requested_payload(WebThreeMethods().get_transaction_count, self.wallet, self.block)
-        return self.response_to_num(self.response_from_request(payload))
+        payload = self._requested_payload(WebThreeMethods().get_transaction_count, self.wallet, self.block)
+        return self._response_to_num(self._response_from_request(payload))
 
     def chain_id(self):
-        payload = self.requested_payload(WebThreeMethods().chan_id)
-        return self.response_to_num(self.response_from_request(payload))
+        payload = self._requested_payload(WebThreeMethods().chan_id)
+        return self._response_to_num(self._response_from_request(payload))
 
     def max_priority_fee_per_gas(self):
-        payload = self.requested_payload(WebThreeMethods().max_priority_fee_per_gas)
-        return self.response_to_num(self.response_from_request(payload))
+        payload = self._requested_payload(WebThreeMethods().max_priority_fee_per_gas)
+        return self._response_to_num(self._response_from_request(payload))
