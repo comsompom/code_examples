2fced918c3ac6f83be254c33dfc0187e242fafad
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-10 09:48:24 +0200

    fix linting in the fast api example

--- simple_fast_api_array/main_page.py
+++ simple_fast_api_array/main_page.py
@@ -1,22 +1,29 @@
-from fastapi import FastAPI
+# pylint: disable=E0401
+# pylint: disable=R1718
+"""Simple example of using the array checking inside the FastAPI"""
 from itertools import permutations
 from collections import Counter
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 def index_page():
+    """the main route to check the GET response from FastAPI"""
     return {'message': 'Success messageFrom Great Fast API example'}
 
 
 @app.post('/arr')
 def check_arr(num_arr):
+    """The route for POST request for return the values from the array"""
     input_arr = [int(x) for x in num_arr.split(',')]
 
     iters = list(permutations(input_arr, 2))
     unique_pairs = set([tuple(sorted(x)) for x in iters])
-    list_double_sums = list({key for key, val in dict(Counter([sum(x) for x in unique_pairs])).items() if val == 2})
-    result_list = [[pair for pair in unique_pairs if sum(pair) == pair_sum] for pair_sum in list_double_sums]
+    list_double_sums = list({key for key, val in
+                             dict(Counter([sum(x) for x in unique_pairs])).items() if val == 2})
+    result_list = [[pair for pair in unique_pairs if sum(pair) == pair_sum]
+                   for pair_sum in list_double_sums]
     return_dict = {}
     for idx, same_pair in enumerate(result_list):
         return_dict[idx] = f"Pairs : {same_pair[0]} {same_pair[1]} have sum : {sum(same_pair[0])}"
