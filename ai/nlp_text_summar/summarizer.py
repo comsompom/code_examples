0304e8275d262c6a6307a61089680ec48511bf83
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-10 10:01:18 +0200

    fix linting for summariser module of the ai nlp

--- ai/nlp_text_summar/summarizer.py
+++ ai/nlp_text_summar/summarizer.py
@@ -1,9 +1,12 @@
+# pylint: disable=E0401
+"""The methods for extract text from web and make summarisation"""
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import requests
 from bs4 import BeautifulSoup
 
 
 def extract_text_from_url(url: str) -> str:
+    """The function for parsing from the Web page the text. Return clean text"""
     raw_page_code = requests.get(url)
     text_from_code = BeautifulSoup(raw_page_code.content, "html.parser")
     paragraphs = text_from_code.find_all("p")
@@ -12,12 +15,20 @@ def extract_text_from_url(url: str) -> str:
 
 
 def summerize_text(input_text: str) -> str:
+    """The function for make summarisation using the AI NLP model. Returns the summarisation"""
     model_name = "t5-small"
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
     # Tokenize and summarize the input text using T5
-    inputs = tokenizer.encode("summarize: " + input_text, return_tensors="pt", max_length=1024, truncation=True)
-    summary_ids = model.generate(inputs, max_length=150, min_length=50, length_penalty=2.0, num_beams=4,
+    inputs = tokenizer.encode("summarize: " + input_text, 
+                              return_tensors="pt", 
+                              max_length=1024, 
+                              truncation=True)
+    summary_ids = model.generate(inputs, 
+                                 max_length=150, 
+                                 min_length=50, 
+                                 length_penalty=2.0, 
+                                 num_beams=4,
                                  early_stopping=True)
 
     # Decode and output the summary
