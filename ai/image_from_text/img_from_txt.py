0d8b574666248edacf4f191b5bf1ec8724389083
comsompom <gelfrad@yandex.com>
2025-03-11 10:43:14 +0200

    fix linting for ai image creator

--- ai/image_from_text/img_from_txt.py
+++ ai/image_from_text/img_from_txt.py
@@ -1,3 +1,5 @@
+# pylint: disable=E0401
+"""Create the generated image from the text prompt"""
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import requests
 from bs4 import BeautifulSoup
@@ -14,6 +16,7 @@ DIFFUSERS_MODEL = "runwayml/stable-diffusion-v1-5"
 
 
 def extract_text_from_url(url: str) -> str:
+    """Get text from provided url"""
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     paragraphs = soup.find_all("p")
@@ -22,17 +25,25 @@ def extract_text_from_url(url: str) -> str:
 
 
 def summarize_text(input_text: str) -> str:
+    """make the text AI suumarization from input text"""
     model_name = TRANSFORMERS_MODEL
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
-    inputs = tokenizer.encode("summarize: " + input_text, return_tensors="pt", max_length=MAX_NEWS_LENGTH, truncation=True)
-    summary_ids = model.generate(inputs, max_length=MAX_SUMMAR_LEN, min_length=50, length_penalty=2.0, num_beams=4,
+    inputs = tokenizer.encode("summarize: " + input_text,
+                              return_tensors="pt",
+                              max_length=MAX_NEWS_LENGTH,
+                              truncation=True)
+    summary_ids = model.generate(inputs,
+                                 max_length=MAX_SUMMAR_LEN,
+                                 min_length=50,
+                                 length_penalty=2.0,
+                                 num_beams=4,
                                  early_stopping=True)
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
 
 def image_creator(prompt: str, neg_prompt: str, img_name: str) -> None:
+    """create the generatiove image from provided test"""
     model_id = DIFFUSERS_MODEL
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to(DEVICE)
@@ -43,8 +54,8 @@ def image_creator(prompt: str, neg_prompt: str, img_name: str) -> None:
 
 text_from_url = extract_text_from_url(URL)
 summary = summarize_text(text_from_url)
-neg_prompt = "nsfw, bad quality, bad anatomy, worst quality, low quality, low resolutions, extra fingers, " \
-             "blur, blurry, ugly, wrongs proportions, watermark, image artifacts, lowres, ugly, jpeg artifacts, " \
-             "deformed, noisy image"
+neg_prompt = "nsfw, bad quality, bad anatomy, worst quality, low quality, low resolutions, " \
+             "extra fingers, blur, blurry, ugly, wrongs proportions, watermark, image " \
+             "artifacts, lowres, ugly, jpeg artifacts, deformed, noisy image"
 
 image_creator(summary, neg_prompt, IMAGE_NAME)
