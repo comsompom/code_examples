72b43ee253ce6e41fbd9ba66b0cdf3e35a16e067
Oleg Bourdo <87123564+comsompom@users.noreply.github.com>
2025-03-12 16:29:19 +0200

    add example of using the gemma hugging face model

---
+++ ai/use_multimodal_gemma/recognize_image.py
@@ -0,0 +1,45 @@
+"""Example of using the Gemma multimodal model with image recognition"""
+import torch
+from transformers import AutoProcessor, Gemma3ForConditionalGeneration
+from huggingface_hub import login
+
+
+with open('hf_token', 'r') as token_file:
+    hf_token = token_file.read()
+
+login(token=hf_token)
+
+
+ckpt = "google/gemma-3-4b-it"
+model = Gemma3ForConditionalGeneration.from_pretrained(
+    ckpt, device_map="auto", torch_dtype=torch.bfloat16,
+)
+processor = AutoProcessor.from_pretrained(ckpt)
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "image": "../entropia_ai/pics/pic1.png"},
+            {"type": "text", "text": "What is drawing on the picture?"}
+        ]
+    }
+]
+inputs = processor.apply_chat_template(
+    messages, add_generation_prompt=True, tokenize=True,
+    return_dict=True, return_tensors="pt"
+).to(model.device)
+
+input_len = inputs["input_ids"].shape[-1]
+
+generation = model.generate(**inputs, max_new_tokens=100, do_sample=False)
+generation = generation[0][input_len:]
+
+decoded = processor.decode(generation, skip_special_tokens=True)
+print(decoded)
+
+
+"""
+the source of the model here:
+https://huggingface.co/blog/gemma3
+"""
